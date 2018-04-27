a2b61ab461e0876504f11aeab6e1ca62381c6665
Samuel Briole <samuel.briole@gmail.com>
2018-04-27 20:09:06 +0100

    some style on the create room form

--- assets/app/components/CreateRoomPage/CreateRoomForm.js
+++ assets/app/components/CreateRoomPage/CreateRoomForm.js
@@ -1,13 +1,32 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { Field } from 'redux-form';
-import { Form, Container, Button } from 'semantic-ui-react';
+import { Form, Container } from 'semantic-ui-react';
 import { FormInput } from 'components';
 import { createRoomRoutine } from 'actions/room';
 import { CREATE_ROOM_FORM } from 'utils/constants/forms';
 import history from 'utils/history';
 
 class CreateRoomForm extends PureComponent {
+  getLabelProps() {
+    const { roomExists } = this.props;
+    return {
+      content: 'jukee.co/room/',
+      color: roomExists ? 'pink' : 'teal',
+    };
+  }
+
+  getSubmitButtonProps() {
+    const { roomExists, submitting } = this.props;
+    return {
+      loading: submitting,
+      labelPosition: 'right',
+      content: roomExists ? 'Join the room' : 'Create a room',
+      icon: roomExists ? 'right arrow' : 'star',
+      color: roomExists ? 'pink' : 'teal',
+    };
+  }
+
   handleSubmit = (event) => {
     const { handleSubmit, urlValue } = this.props;
     handleSubmit(createRoomRoutine)(event)
@@ -19,21 +38,15 @@ class CreateRoomForm extends PureComponent {
   }
 
   render() {
-    const { roomExists, submitting } = this.props;
     return (
       <Container textAlign="center">
         <Form onSubmit={this.handleSubmit}>
           <Field
             name={CREATE_ROOM_FORM.fields.url}
-            label="jukee.co/room/"
+            label={this.getLabelProps()}
             component={FormInput}
-            action={
-              <Button
-                loading={submitting}
-              >
-                {roomExists ? 'Join the room' : 'Create a room'}
-              </Button>
-            }
+            action={this.getSubmitButtonProps()}
+
             placeholder="room name"
           />
         </Form>
