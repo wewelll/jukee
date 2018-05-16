4bda8c388b95e9c3824055d29f55c7a72b26d0e8
Samuel Briole <samuel.briole@gmail.com>
2018-05-16 22:50:29 +0100

    add basic tracklist

--- assets/app/components/Tracklist/index.js
+++ assets/app/components/Tracklist/index.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Container, List, Image } from 'semantic-ui-react';
 
-export default class Tracklist extends Component {
+import { getTracklist } from 'selectors/player';
+
+class Tracklist extends Component {
   render() {
+    const { tracks } = this.props;
     return (
-      <div>tracklist</div>
+      <Container>
+        <List selection verticalAlign="middle">
+          {tracks.map(track => (
+            <List.Item key={track.playerTrackIndex}>
+              <Image avatar src={track.default_thumbnail} />
+              <List.Content>
+                <List.Header>{track.title}</List.Header>
+                <List.Description>{track.channel_title}</List.Description>
+              </List.Content>
+            </List.Item>
+          ))}
+        </List>
+      </Container>
     );
   }
 }
+
+Tracklist.propTypes = {
+  tracks: PropTypes.arrayOf(PropTypes.shape({
+    playerTrackIndex: PropTypes.number.isRequired,
+    default_thumbnail: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    channel_title: PropTypes.string.isRequired,
+  })),
+};
+
+Tracklist.defaultProps = {
+  tracks: [],
+};
+
+const mapStateToProps = state => ({
+  tracks: getTracklist(state),
+});
+
+const PlayerWrapper = connect(mapStateToProps)(Tracklist);
+
+export default PlayerWrapper;
--- assets/app/selectors/player.js
+++ assets/app/selectors/player.js
@@ -1,6 +1,7 @@
 import { createSelector } from 'reselect';
 
 export const getPlayer = state => state.player;
+
 export const getTracklist = createSelector(
   getPlayer,
   player => player.tracks,
