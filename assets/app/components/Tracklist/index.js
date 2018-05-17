8143167f8b03ad03ec63a5f1d8fd4deb62df1582
Samuel Briole <samuel.briole@gmail.com>
2018-05-17 17:31:57 +0100

    display current track in the list

--- assets/app/components/Tracklist/index.js
+++ assets/app/components/Tracklist/index.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Container, List, Image } from 'semantic-ui-react';
 
-import { getTracklist } from 'selectors/player';
-import { sendPlayerEventRoutine, playTrack } from 'actions/player';
+import { getTracklist, getCurrentTrack } from 'selectors/player';
+import { playTrack } from 'actions/player';
 
 class Tracklist extends Component {
   handleTrackClick = playerTrackIndex => () => {
@@ -12,12 +12,13 @@ class Tracklist extends Component {
   }
 
   render() {
-    const { tracks } = this.props;
+    const { tracks, currentTrack } = this.props;
     return (
       <Container>
         <List selection verticalAlign="middle">
           {tracks.map(track => (
             <List.Item
+              active={track.playerTrackIndex === currentTrack.playerTrackIndex}
               key={track.playerTrackIndex}
               onClick={this.handleTrackClick(track.playerTrackIndex)}
             >
@@ -41,19 +42,23 @@ Tracklist.propTypes = {
     title: PropTypes.string.isRequired,
     channel_title: PropTypes.string.isRequired,
   })),
+  currentTrack: PropTypes.shape({
+    playerTrackIndex: PropTypes.number,
+  }),
   playTrack: PropTypes.func.isRequired,
 };
 
 Tracklist.defaultProps = {
   tracks: [],
+  currentTrack: {},
 };
 
 const mapStateToProps = state => ({
   tracks: getTracklist(state),
+  currentTrack: getCurrentTrack(state),
 });
 
 const mapDispatchToProps = {
-  sendPlayerEvent: sendPlayerEventRoutine.request,
   playTrack,
 };
 
