9dcf31e2679bb93f386912a844fb8d00f9ecec8e
Samuel Briole <samuel.briole@gmail.com>
2018-05-27 19:23:33 +0100

    version 0.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Jukee.Mixfile do
   def project do
     [
       app: :jukee,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
