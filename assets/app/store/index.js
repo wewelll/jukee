471c250a61b9c11e2adc57fa4522925de49b269a
Samuel Briole <samuel.briole@gmail.com>
2018-05-29 11:30:04 +0100

    disable redux debugger in prod

--- assets/app/store/index.js
+++ assets/app/store/index.js
@@ -1,34 +1,23 @@
 import 'babel-polyfill';
 import { createStore, applyMiddleware, compose } from 'redux';
-import { createLogger } from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 import { routerMiddleware } from 'react-router-redux';
 
 import reducers from 'reducers';
 
-const loggerMiddleware = createLogger({
-  level: 'info',
-  collapsed: true,
-});
-const useReduxLogger = false;
-
 export const sagaMiddleware = createSagaMiddleware();
 
-/* eslint-disable no-underscore-dangle */
 export default function configureStore(browserHistory) {
   const router = routerMiddleware(browserHistory);
   const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  // const reduxRouterMiddleware = syncHistoryWithStore(browserHistory);
+    process.env.NODE_ENV !== 'production'
+      // eslint-disable-next-line no-underscore-dangle
+      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+      : compose;
 
-  let middleware = [sagaMiddleware, router];
-
-  if (useReduxLogger && process.env.NODE_ENV !== 'production') {
-    middleware = [...middleware, loggerMiddleware];
-  }
+  const middleware = [sagaMiddleware, router];
 
   const createStoreWithMiddleware = composeEnhancers(applyMiddleware(...middleware))(createStore);
 
   return createStoreWithMiddleware(reducers);
 }
-/* eslint-enable */
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Jukee.Mixfile do
   def project do
     [
       app: :jukee,
-      version: "0.0.9",
+      version: "0.0.10",
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
