4f5918338326dc21b2ae29b67bfbea7f3223be9f
Samuel Briole <samuel.briole@gmail.com>
2018-06-03 20:44:57 +0100

    add jukee logo

--- assets/app/components/Layout/Layout.js
+++ assets/app/components/Layout/Layout.js
@@ -3,11 +3,26 @@ import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import routes from 'config/routes';
 import UserDropdown from './UserDropdown';
 import LoginDropdown from './LoginDropdown';
+import JukeeLogo from './jukee_logo.png';
+
+const Container = styled.div`
+  background: #E1E5CF;
+`;
 
 const MainContainer = styled.div`
   min-height: calc(100vh - 56px);
+`;
+
+const JukeeToolbar = styled(Toolbar)`
+  justify-content: space-between;
+`;
+
+const Logo = styled.img`
+  height: 40px;
 `;
 
 export class Layout extends Component {
@@ -17,20 +32,23 @@ export class Layout extends Component {
     } = this.props;
 
     return (
-      <div className={className}>
+      <Container className={className}>
         <AppBar position="static" style={{ background: 'transparent', boxShadow: 'none' }}>
-          <Toolbar>
+          <JukeeToolbar>
+            <Link to={routes.landing}>
+              <Logo src={JukeeLogo} alt="logo" />
+            </Link>
             {isAuthenticated
-            ? <UserDropdown username={username} logout={logout} />
-            : <LoginDropdown />
-          }
-          </Toolbar>
+              ? <UserDropdown username={username} logout={logout} />
+              : <LoginDropdown />
+            }
+          </JukeeToolbar>
         </AppBar>
 
         <MainContainer role="main">
           {children}
         </MainContainer>
-      </div>
+      </Container>
     );
   }
 }
